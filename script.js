7991bb3e26e77d8957668f1920333a6a43dcc2a7
Yiu Ming Wong <56940477+DexterW1@users.noreply.github.com>
2023-09-16 15:17:10 -0700

    added current location

--- script.js
+++ script.js
@@ -1,25 +1,49 @@
 const apiKey = '0b36dbb17bfa775ceb8149f2441436db';
 let userSearchInput = '';
+let lon;
+let lat;
 const searchField= document.querySelector('.searchField');
-const searchBtn = document.querySelector('.search button');
+const searchBtn = document.querySelector('.search button .search-button-icon');
 const updateCity = document.querySelector('.city');
 const updateTemp = document.querySelector('.temp');
 const updateHumidity = document.querySelector('.humidity');
 const updateWind = document.querySelector('.wind');
 const updateWeatherIcon = document.querySelector('.weather-icon');
+const myLocationButton = document.querySelector('.mylocation-button');
 searchField.addEventListener('keydown',(event)=>{
     if(event.key=='Enter'){
         userSearchInput=event.target.value;
-        checkWeather();
+        checkWeather(false);
     }
 })
 searchBtn.addEventListener('click',()=>{
     userSearchInput=searchField.value;
-    checkWeather();
+    checkWeather(false);
 })
 
-async function checkWeather(){
-    const currentWeatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${userSearchInput}&appid=${apiKey}&units=imperial`;
+myLocationButton.addEventListener('click', async () => {
+    // Check if Geolocation is available in the user's browser
+    if ('geolocation' in navigator) {
+        navigator.geolocation.getCurrentPosition(async (position) => {
+            lat = position.coords.latitude;
+            lon = position.coords.longitude;
+            await checkWeather(true);
+        }, (error) => {
+            console.error('Error getting location:', error);
+        });
+    } else {
+        console.error('Geolocation is not available in this browser.');
+    }
+});
+
+async function checkWeather(geolocation){
+    console.log(geolocation);
+    if(geolocation==true){
+        var currentWeatherUrl= `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=imperial`
+    }
+    else{
+        var currentWeatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${userSearchInput}&appid=${apiKey}&units=imperial`;
+    }
     try {
         const response = await fetch(currentWeatherUrl);
         const data = await response.json();
